e9b1e90bc1b802315c55c36cdcba69e547bfde7c
Jonathan Yu <jyu@records.nyc.gov>
2017-11-20 16:06:24 -0500

    Added cron job for backup

--- backup.sh
+++ backup.sh
@@ -5,8 +5,9 @@ DATE=$(date +"%Y-%m-%d")
 LOGFILE="/data/dspace_backup_log-$DATE.log"
 
 source /opt/rh/rh-postgresql95/enable
+# If using vagrant:
 /usr/bin/python /export/local/default/backup.py >> $LOGFILE 2>&1
 
-# If using vagrant run:
-# /usr/bin/python /vagrant/backup.py >> $LOGFILE 2>&1
+# If using SPG server:
+# /usr/bin/python /export/local/default/backup.py >> $LOGFILE 2>&1
 
